3881e628474e436c287230961c90d7856747e386
msamygawad <msamygawad@gmail.com>
2023-06-07 14:53:32 +0300

    solving problems of Action runners by the followin steps: Cleared the repo, cleared all previous commits locally and remotely re-init the repo using Assignments steps re use my previous developed finder.sh and writer.sh push and try again

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -51,7 +51,7 @@ fi
 #echo "Removing the old writer utility and compiling as a native application"
 #make clean
 #make
-
+#mkdir -p "$WRITEDIR"
 for i in $( seq 1 $NUMFILES)
 do
 	./writer.sh "$WRITEDIR/${username}$i.txt" "$WRITESTR"
